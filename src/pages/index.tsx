789d14d014fac87fe231090ed90255523f702d26
MasRockey <indownloader@gmail.com>
2023-03-21 22:41:22 +0800

    update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import Sidebar from './components/sidebar';
-import Main from './components/main';
+
 
 const Index = () => {
   return (
-    <div className="w-full min-h-screen bg-fixed bg-center bg-no-repeat bg-cover bg-homeBg dark:bg-homeTwoBg-dark md:pb-16">
-      <div className="flex items-center justify-center col-span-3">
-        <Sidebar />
-      </div>
-      <div className="col-span-9">
-      <Main />
-      </div>
-    </div>
+    <div className='text-black'>halo dunia</div>
   );
 };
 

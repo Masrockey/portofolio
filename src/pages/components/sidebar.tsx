9d4bad7d6c04e5b95e9471de8f6449c5dbb01846
MasRockey <indownloader@gmail.com>
2023-03-21 22:40:53 +0800

    update all and upload asset

--- src/pages/components/sidebar.tsx
+++ src/pages/components/sidebar.tsx
@@ -1,5 +1,3 @@
-import { faAmbulance } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 function Sidebar(): JSX.Element {
